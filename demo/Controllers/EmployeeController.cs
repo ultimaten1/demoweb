f90bfa656e061217e7ebb2e760e3fdcfdadb9079
Trần Thành Đạt <tthanhdat05102003@gmail.com>
2023-11-28 12:34:22 +0700

    Add project files.

---
+++ demo/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demo.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index(string empSearch)
+        {
+            string mainConn = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+            SqlConnection sqlConn = new SqlConnection(mainConn);
+            string sqlQuery = "SELECT * FROM [dbo].[Employee] WHERE Name LIKE '%" + empSearch + "%'";
+            SqlCommand sqlComm = new SqlCommand(sqlQuery, sqlConn);
+            sqlConn.Open();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlComm);
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+            List<EmpClass> empClasses = new List<EmpClass>();
+
+            foreach (DataRow dataRow in dataSet.Tables[0].Rows)
+            {
+                empClasses.Add(new EmpClass
+                {
+                    Name = Convert.ToString(dataRow["Name"]),
+                    Email = Convert.ToString(dataRow["Email"]),
+                    Salary = Convert.ToString(dataRow["Salary"])
+                });
+            }
+
+            sqlConn.Close();
+            ModelState.Clear();
+            return View(empClasses);
+        }
+    }
+}
